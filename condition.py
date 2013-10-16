7d2ae8b7d4b564b139043d1a6bb3ec352166e368
Matt <mattloppatto@gmail.com>
2013-10-16 12:46:38 -0400

    Code changes

--- condition.py
+++ condition.py
@@ -2,8 +2,8 @@ class Condition(object):
     def __init__(self):
         pass
 
-class PriceCrossesMA(Condition):
-    def __init__(self, ma, above, below):
+class PriceCrossesMovingAverage(Condition):
+    def __init__(self, ma, above=False, below=False):
         self.ma    = ma
         self.above = above
         self.below = below
@@ -12,9 +12,11 @@ class PriceCrossesMA(Condition):
         quote  = quotes[seq_id]
         ma_val = self.ma.calculate(quotes, seq_id)
         if self.above:
-            if quote.open <= ma_val and quote.high > ma_val:
+            if quote.open < ma_val and quote.high > ma_val:
                 return True
         elif self.below:
-            if quote.open >= ma_val and quote.low < ma_val:
+            if quote.open > ma_val and quote.low < ma_val:
                 return True
         return False
+
+class 
--- indicator.py
+++ indicator.py
@@ -3,14 +3,25 @@ class Indicator(object):
         pass
 
 class MovingAverage(Indicator):
-    def __init__(self, period):
+    def __init__(self, period, open=False, high=False, low=False, close=True):
         self.period = period
+        self.open   = open
+        self.high   = high
+        self.low    = low
+        self.close  = close
 
 class SimpleMovingAverage(MovingAverage):
-    def __init__(self, period):
-        super(SimpleMovingAverage, self).__init__(period)
+    def __init__(self, period, open=False, high=False, low=False, close=True):
+        super(SimpleMovingAverage, self).__init__(period, open, high, low, close)
     
     def calculate(self, quotes, seq_id):
         if seq_id < self.period-1:
             raise Exception("Not enough Quotes to calculate a {0} period MA.".format(self.period))
-        return sum([q.close for q in quotes[seq_id-self.period+1:seq_id+1]])/self.period
+        if self.open:
+            return sum([q.open for q in quotes[seq_id-self.period+1:seq_id+1]])/self.period
+        elif self.high:
+            return sum([q.high for q in quotes[seq_id-self.period+1:seq_id+1]])/self.period
+        elif self.low:
+            return sum([q.low for q in quotes[seq_id-self.period+1:seq_id+1]])/self.period
+        else: # self.close
+            return sum([q.close for q in quotes[seq_id-self.period+1:seq_id+1]])/self.period
